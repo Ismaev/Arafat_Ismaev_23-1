df5348b289244385b2c5a18a2f355f56e972c8c1
Ismaev <arafat.ismaev@gmail.com>
2023-02-25 21:16:36 +0600

    second hw

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,7 +1,12 @@
+
 from django.db import models
 
 class Director(models.Model):
     name = models.CharField(max_length=50, verbose_name='Имя директора')
+
+    @property
+    def movie_count(self):
+        return self.movie_set.all().count()
     def __str__(self):
         return self.name
 
@@ -10,12 +15,25 @@ class Movie(models.Model):
     description = models.TextField(max_length=1000, verbose_name='Описание')
     duration = models.IntegerField(default=0, verbose_name='Длительность')
     director = models.ForeignKey(Director, on_delete=models.CASCADE)
+
+    @property
+    def rating(self):
+        total_amount = self.review_set.all().count()
+        if total_amount == 0:
+            return 0
+        sum_ = 0
+        for i in self.review_set.all():
+            sum_ += i.stars
+        return sum_ / total_amount
+
+
     def __str__(self):
         return self.title
 
 class Review(models.Model):
     text = models.TextField(max_length=1000, verbose_name='Комментарий')
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    stars = models.PositiveIntegerField(max_length=5, default='Звезды')
     def __str__(self):
         return self.text
 
--- movie_app/urls.py
+++ movie_app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("api/v1/movies/<int:id>/", views.get_movie),
     path("api/v1/reviews/", views.review_list),
     path("api/v1/reviews/<int:id>/", views.get_review),
+    path("api/v1/movies/reviews/", views.movie_list),
+
 ]
